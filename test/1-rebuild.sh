023fdb7e3ce17c3e0dee476c3c5c06b24fafb99b
GollyTicker <golly.ticker@gmail.com>
2023-02-02 16:24:47 +0100

    test wasm: basic wasm managed array

--- test/1-rebuild.sh
+++ test/1-rebuild.sh
@@ -3,6 +3,7 @@ set -eu
   cd test/array
   # requires installation of rust+wasm first:
   # https://developer.mozilla.org/en-US/docs/WebAssembly/Rust_to_wasm
+  rm -rf pkg/*
   wasm-pack build --target web
   wasm2wat pkg/array_bg.wasm -o array.wat
 
--- test/array/src/lib.rs
+++ test/array/src/lib.rs
@@ -1,13 +1,36 @@
 use wasm_bindgen::prelude::*;
 
+const WASM_MEMORY_BUFFER_SIZE: usize = 2;
+
+// This memory is allocated inside WASM - and the host
+// will copy contents into it, so that wasm can work with it.
+// This syntax [T; N] means a fixed-size array
+// of elements of type T of length N.
+static mut WASM_MEMORY_BUFFER: [u8; WASM_MEMORY_BUFFER_SIZE] = [0; WASM_MEMORY_BUFFER_SIZE];
+
+// Example usage functions
+
 #[wasm_bindgen]
-// This actually references the external javascript alert function.
-extern {
-    pub fn alert(s: &str);
+pub fn store_value_in_wasm_memory_buffer(x: u8) {
+    unsafe {
+        WASM_MEMORY_BUFFER[0] = x;
+    }
+}
+
+// The pointer in wasm-memory-space to our above buffer.
+// Needed for the host to write data into it.
+#[wasm_bindgen]
+pub fn get_wasm_memory_buffer_ptr() -> *const u8 {
+    let ptr: *const u8;
+    unsafe {
+        ptr = WASM_MEMORY_BUFFER.as_ptr();
+    }
+    return ptr;
 }
 
 #[wasm_bindgen]
-// We want this function to be accessible from outside.
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
+pub fn read_wasm_memory_buffer() -> u8 {
+    unsafe {
+        return WASM_MEMORY_BUFFER[0];
+    }
 }
